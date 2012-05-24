0e2ca258a3dec9897007502ba015a6af84e5dd82
Joshua Jansen <joshua@itflows.nl>
2012-05-24 22:41:35 +0200

    Calculate points method on team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,24 @@
 class Team < ActiveRecord::Base
   attr_accessible :name, :poule_id
+  attr_accessor :points
   
   belongs_to :poule
+  has_many :home_games, :class_name => 'Game', :foreign_key => 'home_id'
+  has_many :away_games, :class_name => 'Game', :foreign_key => 'away_id'
+  
+  def calulate_points
+    p = 0
+    self.games.each do |game|
+      if game.winner == self.id
+        p = p + 3
+      elsif game.winner == 0
+        p = p + 1
+      end
+    end
+    return p
+  end
+  
+  def games
+     self.home_games + self.away_games
+  end
 end
